05cbebdbb133af3b06cd55e3d05db31cb7b4f290
Stephen Ombuya <129269603+stephenombuya@users.noreply.github.com>
2025-01-20 16:17:41 +0300

    Update main.rs

--- src/system-service/src/main.rs
+++ src/system-service/src/main.rs
@@ -1,1 +1,20 @@
+use tokio::time::{sleep, Duration};
+use battery::Manager;
 
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let manager = Manager::new()?;
+    
+    loop {
+        if let Ok(batteries) = manager.batteries() {
+            for battery in batteries {
+                if let Ok(battery) = battery {
+                    println!("Battery level: {:.2}%", 
+                            battery.state_of_charge().value * 100.0);
+                    println!("Status: {}", battery.state());
+                }
+            }
+        }
+        sleep(Duration::from_secs(60)).await;
+    }
+}
