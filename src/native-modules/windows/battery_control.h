581e3e9448e9d99017139f0057fa630512547cfa
Stephen Ombuya <129269603+stephenombuya@users.noreply.github.com>
2025-01-20 16:08:23 +0300

    Update battery_control.h

--- src/native-modules/windows/battery_control.h
+++ src/native-modules/windows/battery_control.h
@@ -1,1 +1,9 @@
+#pragma once
+#include <windows.h>
 
+extern "C" {
+    __declspec(dllexport) bool initialize_battery_control();
+    __declspec(dllexport) int get_battery_level();
+    __declspec(dllexport) bool is_charging();
+    __declspec(dllexport) bool set_charging_state(bool enable);
+}
