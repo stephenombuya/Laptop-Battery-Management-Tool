c889c9a219602f180d5378460d65947a0ce6a31b
Stephen Ombuya <129269603+stephenombuya@users.noreply.github.com>
2025-01-20 16:15:15 +0300

    Update main.js

--- src/desktop-app/src/main.js
+++ src/desktop-app/src/main.js
@@ -1,1 +1,28 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+const { spawn } = require('child_process');
 
+let mainWindow;
+let pythonProcess;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
+
+    mainWindow.loadFile('src/index.html');
+}
+
+app.whenReady().then(() => {
+    createWindow();
+    startPythonService();
+});
+
+function startPythonService() {
+    pythonProcess = spawn('python', ['-m', 'battery_manager']);
+}
