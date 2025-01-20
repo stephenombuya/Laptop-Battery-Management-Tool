8a9d75f30ce56d8d38096989da358467cb3f8d9b
Stephen Ombuya <129269603+stephenombuya@users.noreply.github.com>
2025-01-20 16:18:22 +0300

    Update battery.rs

--- src/system-service/src/battery.rs
+++ src/system-service/src/battery.rs
@@ -1,1 +1,19 @@
+use battery::Battery;
+use serde::Serialize;
 
+#[derive(Serialize)]
+pub struct BatteryStatus {
+    level: f32,
+    charging: bool,
+    time_remaining: Option<u64>,
+}
+
+impl BatteryStatus {
+    pub fn from_battery(battery: &Battery) -> Self {
+        Self {
+            level: battery.state_of_charge().value * 100.0,
+            charging: battery.state().is_charging(),
+            time_remaining: None, // Implement time calculation
+        }
+    }
+}
