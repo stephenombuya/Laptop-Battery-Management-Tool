ecd611d29e99f4957c022c55e42863ac6823f31a
Stephen Ombuya <129269603+stephenombuya@users.noreply.github.com>
2025-01-21 16:01:01 +0300

    Delete src/__init__.py

--- src/__init__.py
+++
@@ -1,5 +0,0 @@
-from .core import BatteryMonitor, BatteryObserver, ChargingController
-from .utils import get_platform_info, setup_logging
-
-__version__ = "1.0.0"
-__all__ = ['BatteryMonitor', 'BatteryObserver', 'ChargingController']
